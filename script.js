4d3506894df62d664bcdf760304e7590f522c941
HeitorDalla <heitorvillavilla@gmail.com>
2025-04-06 20:48:40 -0300

    Feito a primeira logica do projeto de converter tudo que nao seja numero para vazio

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    event.preventDefault();
+
+    document.querySelector("#cpf").focus(); // Assim que a pagina atualizar, esta dando foco ao input
+
+    const inputCpf = document.querySelector("#cpf");
+    inputCpf.addEventListener("input", (event) => {
+        let valor = event.target.value.replace(/\D/g, ''); // Converte todos os caracteres que não sejam números para 'vazio'
+        
+    });
+
+});
